8148265c11b237a0ba65cc731e9c7ce3a149c77d
stevethewright <stevethewright@live.com>
2023-02-22 20:23:41 +1030

    Updated eb setenv.

--- 4_fullstack_application/udagram/udagram-frontend/bin/deploy.sh
+++ 4_fullstack_application/udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,11 @@
 aws s3 cp --recursive --acl public-read ./www s3://udagram-bucket-786172554301/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-bucket-786172554301/
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv AWS_BUCKET=$AWS_BUCKET
+eb setenv AWS_REGION=$AWS_REGION
+eb setenv AWS_PROFILE=$AWS_PROFILE
+eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv URL=$URL
